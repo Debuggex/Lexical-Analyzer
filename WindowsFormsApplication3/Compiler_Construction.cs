e58f129843843779e61aa417f587d56a04034a82
Sahbaan <sahbaanalam34@gmail.com>
2021-02-28 14:55:50 +0500

    Lexical Analyzer

---
+++ WindowsFormsApplication3/Compiler_Construction.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    class Compiler_Construction
+    {
+        public string[] Word_Separator(String code)
+        {
+            string[] linesofcode = code.Split(';');
+            return linesofcode;
+        }
+
+        public void is_IDENTIFIER_or_KEYWORD(string word, int linenumber)
+        {
+            if (word != null)
+            {
+                Regex Alphabet = new Regex("^[a-zA-z]+$");
+                bool AlphabetAnswer = Alphabet.IsMatch(word);
+                
+                Regex Integer = new Regex("^[0-9]+$");
+                bool isINTEGER = Integer.IsMatch(word);
+                Regex Float = new Regex("^[0-9]+[.][0-9]+(e)?[0-9]+$");
+                bool isFLOAT = Float.IsMatch(word);
+                Regex STRING = new Regex("^\"[a-z]*[A-Z]*[0-9]*\"$");
+                bool isSTRING = STRING.IsMatch(word);
+                Regex Identifier = new Regex("^(([A-Za-z]||[_])+([A-Za-z]||[0-9]||[_])*([A-Za-z]||[0-9])||[A-Za-z])$");
+                bool isIDENTIFIER = Identifier.IsMatch(word);
+                if (AlphabetAnswer == true)
+                {
+
+
+                    string[] classvalue = KeyWords(word);
+                    if (classvalue != null)
+                    {
+                        string token = "( " + classvalue[0] + " , " + classvalue[1] + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+
+
+                    }
+                    else
+                    {
+                        string token = "( " + "Identifier" + " , " + word + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+                    }
+
+
+                }
+                else
+                {
+                    
+                    if (isSTRING==true)
+                    {
+                         string token = "( " + "String Constatnt" + " , " + word + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+                    }
+                    else if (isINTEGER==true)
+                    {
+                         string token = "( " + "Integer Constant" + " , " + word + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+                    }
+                    else if (isFLOAT==true)
+                    {
+                         string token = "( " + "Float Constant" + " , " + word + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+                    }
+                    else if (isIDENTIFIER == true)
+                    {
+                        string token = "( " + "Identifier" + " , " + word + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+
+
+                    }
+                    else
+                    {
+                        string token = "( " + "Invalid Token" + " , " + word + " , " + linenumber.ToString() + " )";
+                        File.AppendAllText("Token.txt", token + Environment.NewLine);
+                    }
+
+
+                }
+
+
+            }
+        }
+
+
+        public string[] KeyWords(string word)
+        {
+            
+            string [][] classes=new string[26][];
+
+            classes[0] = new string[3] { "I/O", "input", "output" };
+            classes[1] = new string[2]{"format","format"};
+            classes[2] = new string[2] { "break", "break" };
+            classes[3] = new string[2] { "class", "class" };
+            classes[4] = new string[2] { "continue", "continue" };
+            classes[5] = new string[2] { "function", "func" };
+            classes[6] = new string[3] { "True/False", "true", "false" };
+            classes[7] = new string[2] { "while", "while" };
+            classes[8] = new string[2] { "else", "else" };
+            classes[9] = new string[2] { "elif", "elif" };
+            classes[10] = new string[2] { "for", "for" };
+            classes[11] = new string[2] { "from", "from" };
+            classes[12] = new string[3] { "empty", "empty", "null" };
+            classes[13] = new string[2] { "take", "take" };
+            classes[14] = new string[2] { "have", "have" };
+            classes[15] = new string[2] { "cmp_equal", "cmp_equal" };
+            classes[16] = new string[3] { "out", "out", "return" };
+            classes[17] = new string[2] { "to", "to" };
+            classes[18] = new string[2] { "with", "with" };
+            classes[19] = new string[2] { "new", "new" };
+            classes[20] = new string[7] { "datatypes", "int", "double", "float", "string", "object", "char" };
+            classes[21] = new string[2] { "static", "static" };
+            classes[22] = new string[2] { "if", "if" };
+            classes[23] = new string[2] { "dowhile", "dowhile" };
+            classes[24] = new string[2] { "array", "array" };
+            classes[25] = new string[2] { ".", "." };
+            
+            for (int i = 0; i < classes.Length; i++)
+            {
+                for (int j = 0; j < classes[i].Length; j++)
+                {
+                    if (classes[i][j]==word)
+                    {
+                        string classpart = classes[i][0];
+                        string valuepart = word;
+                        string []classvalue=new string[2]{classpart,valuepart};
+                        return classvalue;
+                    }
+                }
+            }
+            return null;
+            
+        }
+
+
+
+        
+    }
+
+    
+}
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        Compiler_Construction CC = new Compiler_Construction();
+       
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            
+            string[] linesofcode = File.ReadAllLines("Code.txt");
+            if (File.Exists("Token.txt"))
+            {
+                File.WriteAllText("Token.txt","");
+            }
+
+            for (int i = 0; i < linesofcode.Length; i++)
+            {
+                string codeline = linesofcode[i];
+                int linenumber = i + 1;
+                string word = null;
+                string secondword = null;
+                for (int j = 0; j < codeline.Length; j++)
+                {
+                    int invertedcomma = 0;
+                    if (codeline[j]=='"')
+                    {
+
+                        invertedcomma++;
+
+                        if (invertedcomma==1)
+                        {
+                            if (word != null)
+                            {
+                                CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                                word = null;
+                                word = word + codeline[j];
+                                
+                            }
+                            word = word + codeline[j];
+
+                        }
+                        while (invertedcomma==1&&j!=codeline.Length-1)
+                        {
+                            j++;
+                            word = word + codeline[j];
+                            if (codeline[j]=='"')
+                            {
+                                invertedcomma++;
+                                
+                                CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                                word = null;
+                            }
+                        }
+                        continue;
+                        
+                    }
+                    if (codeline[j]=='.')
+                    {
+                        Regex number = new Regex("^[0-9]+$");
+                        bool isnumber = number.IsMatch(word);
+                        if (isnumber==true)
+                        {
+                            j++;
+                            Regex numbercharacter = new Regex("^[0-9]*$");
+                            isnumber=numbercharacter.IsMatch(codeline[j].ToString());
+                            while (codeline[j]!=' '||codeline[j]!=';')
+                            {
+                                secondword = secondword + codeline[j];
+                                if (j!=codeline.Length-1)
+                                {
+                                    j++;
+                                    isnumber = numbercharacter.IsMatch(codeline[j].ToString());
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                                
+                            }
+                            CC.is_IDENTIFIER_or_KEYWORD(word + "." + secondword, linenumber);
+                            word = null;
+                            if (j != codeline.Length - 1)
+                            {
+                                j++;
+                                word = word + codeline[j];
+                            }
+                            
+                            secondword = null;
+                            continue;
+                        }
+                        else
+                        {
+                            CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                            string punctuatortoken = "( ., " + codeline[j] + ", " + linenumber.ToString() + ")";
+
+                            File.AppendAllText("Token.txt", punctuatortoken + Environment.NewLine);
+                            word = null;
+                            continue;
+                        }
+                    }
+
+                    if (codeline[j] == '(' || codeline[j] == ')' || codeline[j] == '[' || codeline[j] == ']' || codeline[j] == '{' || codeline[j] == '}' || codeline[j] == '\'' || codeline[j] == '\\' || codeline[j] == ',')
+                    {
+
+                        if (word!=null)
+                        {
+                            CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                        }
+                            
+                         string punctuatortoken = "( Punctuator, " + codeline[j] + ", " + linenumber.ToString()+")";
+
+                            File.AppendAllText("Token.txt", punctuatortoken + Environment.NewLine);
+                            word = null;
+                            continue;
+
+
+
+                    }
+                    else if(codeline[j]=='=')
+                    {
+                        if (word!=null)
+                        {
+                            CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                        }
+                        if (j != codeline.Length - 1)
+                        {
+                            if (codeline[j + 1] == '=')
+                            {
+                                string compoundassignment = "( Relational Operator, " + codeline[j] + codeline[j + 1] + ", " + linenumber.ToString() + " )"+Environment.NewLine;
+                                File.AppendAllText("Token.txt", compoundassignment);
+                                j++;
+                                word = null;
+                                continue;
+                            }
+
+                        }
+                        
+                        
+                            string punctuatortoken = "( Assignment Operator, " + codeline[j] + ", " + linenumber.ToString() + ")";
+
+                            File.AppendAllText("Token.txt", punctuatortoken + Environment.NewLine);
+                            word = null;
+                            continue;
+                        
+                        
+                    }
+                    else if (codeline[j] == '+' || codeline[j] == '-' || codeline[j] == '*' || codeline[j] == '/' || codeline[j] == '%' || codeline[j] == '^')
+                    {
+                        if (word != null)
+                        {
+                            CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                        }
+                        if (j!=codeline.Length-1&&codeline[j]!='^')
+                        {
+                            if (codeline[j]==codeline[j+1])
+                            {
+                                string compoundassignment = "( Compount Assignment, " + codeline[j] + codeline[j + 1] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                                File.AppendAllText("Token.txt", compoundassignment);
+                                j++;
+                                word = null;
+                                continue;
+                            }
+                            
+                        }
+                        if (codeline[j] == '+' || codeline[j] == '-')
+                        {
+                            string compoundassignment = "( PM, " + codeline[j] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                            File.AppendAllText("Token.txt", compoundassignment);
+
+                            word = null;
+                            continue;
+                        }
+                        else
+                        {
+                            string compoundassignment = "( MDME, " + codeline[j] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                            File.AppendAllText("Token.txt", compoundassignment);
+
+                            word = null;
+                            continue;
+                        }
+                        
+                    }
+                    else if (codeline[j] == '>' || codeline[j] == '<' || codeline[j] == '!')
+                    {
+                        if (word != null)
+                        {
+                            CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                        }
+                        if (j != codeline.Length - 1 && codeline[j] != '!')
+                        {
+                            if (codeline[j+1] == '=')
+                            {
+                                string compoundassignment = "( Relational Operator, " + codeline[j] + codeline[j + 1] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                                File.AppendAllText("Token.txt", compoundassignment);
+                                j++;
+                                word = null;
+                                continue;
+                            }
+
+                        }
+                        if (codeline[j] == '!')
+                        {
+                            if (j != codeline.Length - 1)
+                            {
+                                if (codeline[j + 1] == '=')
+                                {
+                                    string compoundassignment = "( Relational Operator, " + codeline[j] + codeline[j + 1] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                                    File.AppendAllText("Token.txt", compoundassignment);
+                                    j++;
+                                    word = null;
+                                    continue;
+                                }
+                                else
+                                {
+                                    string compoundassignment = "( Invalid Token, " + codeline[j] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                                    File.AppendAllText("Token.txt", compoundassignment);
+
+                                    word = null;
+                                    continue;
+                                }
+                            }
+
+
+                        }
+                        else
+                        {
+                            string compoundassignment = "( Relational Operator, " + codeline[j] + ", " + linenumber.ToString() + " )" + Environment.NewLine;
+                            File.AppendAllText("Token.txt", compoundassignment);
+
+                            word = null;
+                            continue;
+                        }
+                        
+                    }
+                    else if (codeline[j]!=' '&&codeline[j]!=';')
+                    {
+                        word = word + codeline[j];
+                        continue;
+                    }
+                    
+                    else
+                    {
+                        CC.is_IDENTIFIER_or_KEYWORD(word, linenumber);
+                        
+                        word = null;
+                    }
+                    
+                    
+                }
+                if (word!=null)
+                {
+                    CC.is_IDENTIFIER_or_KEYWORD(word,linenumber);
+
+                }
+
+            }
+            MessageBox.Show("Token Creation Successfully");
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            File.WriteAllText("Code.txt", textBox1.Text);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string mytext=File.ReadAllText("Token.txt");
+            textBox1.Text = "";
+            textBox1.Text = mytext;
+
+        }
+
+        public int linenumber { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
